a39bd2563d96e6b37e31a011f0bfccf739a54a72
iago bruno <iago.silva6969@gmail.com>
2025-07-02 16:54:23 -0300

    +

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,49 @@
 display = document.querySelector('#display');
 
 function calcular(valor, tipo) {
+    const calculadora = new Calculadora(valor, tipo);
+}
+
+function Calculadora(valor, tipo) {
+    this.valor = valor;
+    this.tipo = tipo;
+
+    if (this.tipo === 'acao') {
+
+        if(this.valor === 'c') {
+            display.value = '';
+
+        } else if(this.valor === '<') {
+            display.value = display.value.slice(0, -1);
+
+        } else if(this.valor === '=') {
+
+            try {  
+                display.value = eval(display.value);
+            } catch (e) {
+                display.value = '';
+                alert('Erro na expressão');
+            }   
+
+        } else {
+            display.value += this.valor;
+        }
+
+    } else if (this.tipo === 'num') {
+        display.value += this.valor;
+    }
+}
+
+
+
+
+
+
+
+
+
+/*
+function calcular(valor, tipo) {
     if (tipo === 'acao') {
 
         if (valor === 'c') {
@@ -23,4 +66,4 @@ function calcular(valor, tipo) {
     } else if (tipo === 'num') {
         display.value += valor;
     }
-}
+} */

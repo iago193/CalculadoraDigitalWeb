2ea0f658c62f96496efc876526de0c64ae466828
iago bruno <iago.silva6969@gmail.com>
2025-07-04 17:07:21 -0300

    +

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,8 @@ function calcular(valor, tipo) {
     const calculadora = new Calculadora(valor, tipo);
 }
 
+// Classe Calculadora que manipula o display da calculadora
+
 function Calculadora(valor, tipo) {
     this.valor = valor;
     this.tipo = tipo;
@@ -22,7 +24,7 @@ function Calculadora(valor, tipo) {
                 display.value = eval(display.value);
             } catch (e) {
                 display.value = '';
-                alert('Erro na expressão');
+                alert('Erro na expressão:'+ ' ' + e.message);
             }   
 
         } else {
@@ -35,14 +37,70 @@ function Calculadora(valor, tipo) {
 }
 
 
-
-
-
-
-
-
-
 /*
+
+// Versão alternativa da função Calculadora usando uma classe
+
+function Calculadora() {
+  this.display = document.querySelector('.display');
+
+  this.inicia = () => {
+    this.capturaCliques();
+    this.capturaEnter();
+  };
+
+  this.capturaEnter = () => {
+    document.addEventListener('keyup', e => {
+      if (e.keyCode === 13) {
+        this.realizaConta();
+      }
+    });
+  };
+
+  this.capturaCliques = () => {
+    document.addEventListener('click', event => {
+      const el = event.target;
+      if (el.classList.contains('btn-num')) this.addNumDisplay(el);
+      if (el.classList.contains('btn-clear')) this.clear();
+      if (el.classList.contains('btn-del')) this.del();
+      if (el.classList.contains('btn-eq')) this.realizaConta();
+    });
+  };
+
+  this.realizaConta = () => {
+    try {
+      const conta = eval(this.display.value);
+
+      if(!conta) {
+        alert('Conta inválida');
+        return;
+      }
+
+      this.display.value = conta;
+    } catch(e) {
+      alert('Conta inválida');
+      return;
+    }
+  };
+
+  this.addNumDisplay = el => {
+    this.display.value += el.innerText;
+    this.display.focus();
+  };
+
+  this.clear = () => this.display.value = '';
+  this.del = () => this.display.value = this.display.value.slice(0, -1);
+}
+
+const calculadora = new Calculadora();
+calculadora.inicia();
+
+
+
+
+
+// Versão alternativa da função calcular usando functional programming
+
 function calcular(valor, tipo) {
     if (tipo === 'acao') {
 
